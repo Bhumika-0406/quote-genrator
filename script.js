006270e60d2ac712698d260fd40de59b8389af37
Bhumika Pandey <pandeybhumika0406@gmail.com>
2025-06-17 01:02:10 +0530

    This is a quote generator

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiUrl="http://api.quotable.io/random";
+
+const quote=document.getElementById("quote");
+const author=document.getElementById("author");
+
+async function getquote(url) {
+    const response = await fetch(url);
+    let data =await response.json();
+    console.log(data);
+    quote.innerHTML = data.content;
+     author.innerHTML = data.author;
+}
